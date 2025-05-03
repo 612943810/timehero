6e32581ff0ee471a0ee19b952550c3a24cd4a3f7
Raiyan Rofiquzzaman <raiyanrofiquizzaman@gmail.com>
2025-05-03 16:04:28 -0400

    This update creases a timer in my app.

--- timehero/src/App.tsx
+++ timehero/src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import './App.css'
-import Navbar from './Navbar'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Navbar from './Navbar';
 
 function App() {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <>
-     <Navbar /> 
+      <Navbar />
+      <div className="timer-container">
+        <h1 className="timer-title">Timer</h1>
+        <p className="timer-value">{time} seconds</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
