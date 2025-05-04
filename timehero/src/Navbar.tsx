6738df5517231a31c1512c908fa3798fa82f0d05
Raiyan Rofiquzzaman <raiyanrofiquizzaman@gmail.com>
2025-05-04 14:54:07 -0400

    This update fixes active link.

--- timehero/src/Navbar.tsx
+++ timehero/src/Navbar.tsx
@@ -1,19 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <div className="navbar-content">
         <h1>TimeHero</h1>
         <div className="nav-links">
           <Link to="/" className="nav-link" style={{
-            color: window.location.pathname === '/' ? '#ffffff' : '#666666'
+            color: location.pathname === '/' ? '#ffffff' : '#666666'
           }}>
             Timer
           </Link>
           <Link to="/about" className="nav-link" style={{
-            color: window.location.pathname === '/about' ? '#ffffff' : '#666666'
+            color: location.pathname === '/about' ? '#ffffff' : '#666666'
           }}>
             About
           </Link>
